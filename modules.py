94d1e5f0eb905f865cce1dd5b3c083a3e6d5be17
Sami-Shafi <freelancersami123@gmail.com>
2022-07-28 04:27:21 +0600

    virtual Environment created

--- modules.py
+++ modules.py
@@ -1,8 +1,15 @@
 # A module is basically a file containing a set of functions to include in your application.
 # There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
 
+# native modules
 from datetime import date
 from time import time
 
-print(date.today())
-print(time())
+# pip
+from camelcase import CamelCase
+
+# print(date.today())
+# print(time())
+
+c = CamelCase()
+print(c.hump("hello there!"))
